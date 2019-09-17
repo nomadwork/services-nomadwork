d53de67e1a0bac5b4a7026ce830daaae15e4458f
caiobgs <caiobgs2@gmail.com>
2019-09-17 20:20:08 -0300

    token creation

---
+++ Model View/Login/UserLogin.cs
@@ -0,0 +1,9 @@
+namespace Nomadwork.Infra.Models
+{
+    public class UserLogin
+    {
+       public string Email { get; set; } 
+       public string Password { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,27 +1,64 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Nomadwork.Infra.Data;
 
 namespace Nomadwork
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddResponseCompression();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                                                   options.UseMySql(
+                                                   Configuration.GetConnectionString("DbConnectionLocal")));
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "nomadwork.com.br",
+                    ValidAudience = "nomadwork.com.br",
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("Token inválido..:. " + context.Exception.Message);
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("Toekn válido...: " + context.SecurityToken);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
             services.AddSwaggerGen(x =>
             {
