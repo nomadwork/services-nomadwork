fa9c4a40bc55e318019e84680cca8390f3643eb5
caiobgs <caiobgs2@gmail.com>
2019-10-01 17:57:23 -0300

    user module

---
+++ Infra/Data/ObjectData/UserModelData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomadwork.Infra.Data.ObjectData
+{
+    public class UserModelData : AEntity
+    {
+        [Required, Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+        [Required, Column(TypeName = "varchar(50)")]
+        public string Email { get; set; }
+
+        [Required, Column(TypeName = "varchar(100)")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        public  string Dateborn { get; set; }
+    }
+}
---
+++ Infra/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Nomadwork.Infra.Data.Contexts;
+using Nomadwork.Infra.Data.ObjectData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomadwork.Infra.Repository
+{
+    public class UserRepository
+    {
+        private readonly NomadworkDbContext _context;
+
+        private UserRepository(NomadworkDbContext context)
+        {
+            _context = context;
+        }
+
+        public static UserRepository GetInstance(NomadworkDbContext context)
+            => new UserRepository(context);
+
+        public UserModelData GetByEmail(string email)
+             => _context.Users
+                                    
+                                    .FirstOrDefault(user => user.Email.Equals(email) && user.Active);
+    }
+}
