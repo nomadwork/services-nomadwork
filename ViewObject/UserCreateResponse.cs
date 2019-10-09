507385a21b3b09602877a3987789f660e051c2a4
caiobgs <caiobgs2@gmail.com>
2019-10-08 22:48:57 -0300

    user viewobject

---
+++ ViewObject/UserCreateResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomadwork.ViewObject
+{
+    public class UserCreateResponse
+    {
+        public bool status { get; set; }
+        public string mensage { get; set; }
+    }
+}
---
+++ ViewObject/UserCreateToUserModelData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomadwork.ViewObject
+{
+    public class UserCreateToUserModelData
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Dateborn { get; set; }
+
+        public int Gender { get; set; }
+    }
+}
