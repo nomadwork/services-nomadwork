1560eb59c1a1bdc9689d856f2d438238258a309c
caiobgs <caiobgs2@gmail.com>
2019-11-25 18:24:22 -0300

    logRepository fix

--- Infra/Repository/LogRepository.cs
+++ Infra/Repository/LogRepository.cs
@@ -31,14 +31,14 @@ namespace Nomadwork.Repository
                             .ToListAsync();
         }
 
-        public async Task<IEnumerable<EstablishmmentDetailsScheme>> GetEstablishmmentDetail(long establishmmentId)
+        public async Task<EstablishmmentDetailsScheme> GetEstablishmmentDetail(long establishmmentId)
         {
             FilterDefinition<EstablishmmentDetailsScheme> filter = Builders<EstablishmmentDetailsScheme>.Filter.Eq(m => m.EstablishmmentId, establishmmentId);
 
             return await _context
                     .Establishmentcollection
                     .Find(filter)
-                    .ToListAsync();
+                    .FirstOrDefaultAsync();
 
             
              
@@ -48,52 +48,6 @@ namespace Nomadwork.Repository
             //.FirstOrDefaultAsync();
         }
 
-        //public async Task<IEnumerable<GameModelData>> GetAllGames()
-        //{
-        //    return await _context
-        //                    .Games
-        //                    .Find(_ => true)
-        //                    .ToListAsync();
-        //}
-
-        //public Task<GameModelData> GetGame(string name)
-        //{
-        //    FilterDefinition<GameModelData> filter = Builders<GameModelData>.Filter.Eq(m => m.Name, name);
-
-        //    return _context
-        //            .Games
-        //            .Find(filter)
-        //            .FirstOrDefaultAsync();
-        //}
-
-        //public async Task Create(GameModelData game)
-        //{
-        //    await _context.Games.InsertOneAsync(game);
-        //}
-
-        //public async Task<bool> Update(GameModelData game)
-        //{
-        //    ReplaceOneResult updateResult =
-        //        await _context
-        //                .Games
-        //                .ReplaceOneAsync(
-        //                    filter: g => g.Id == game.Id,
-        //                    replacement: game);
-
-        //    return updateResult.IsAcknowledged
-        //            && updateResult.ModifiedCount > 0;
-        //}
-
-        //public async Task<bool> Delete(string name)
-        //{
-        //    FilterDefinition<GameModelData> filter = Builders<GameModelData>.Filter.Eq(m => m.Name, name);
-
-        //    DeleteResult deleteResult = await _context
-        //                                        .Games
-        //                                        .DeleteOneAsync(filter);
-
-        //    return deleteResult.IsAcknowledged
-        //        && deleteResult.DeletedCount > 0;
-        //}
+        
     }
 }
